324c63a74deac601a6aa581cc5f3910a7319f79d
Chimzi Chiorlu <chimzi@Chimzi-MacBook-Pro.local>
2022-07-10 18:19:41 +0100

    scaffold identity pages

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ChealCore.Data;
 using ChealCore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 // Add services to the container.
 var connectionString = builder.Configuration["ConnectionStrings:ChealCore"];
