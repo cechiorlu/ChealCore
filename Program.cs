923589b6f22f55ffe60b2c99ba831fb2bf16b1c1
Chimzi Chiorlu <chimzi@Chimzi-MacBook-Pro.local>
2022-07-10 17:42:47 +0100

    setup db connection

--- Program.cs
+++ Program.cs
@@ -5,9 +5,8 @@ using ChealCore.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+var connectionString = builder.Configuration["ConnectionStrings:ChealCore"];
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -43,3 +42,87 @@ app.MapRazorPages();
 
 app.Run();
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using ChealCBA.Data;
+//using ChealCBA.Models;
+//using EmailService;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//var connectionString = builder.Configuration["ConnectionStrings:CBAConnection"];
+//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+//{
+//    options.SignIn.RequireConfirmedAccount = true;
+//    options.Lockout.AllowedForNewUsers = true;
+//    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+//    options.Lockout.MaxFailedAccessAttempts = 3;
+//    options.User.RequireUniqueEmail = true;
+//})
+//    .AddEntityFrameworkStores<ApplicationDbContext>()
+//    .AddDefaultTokenProviders();
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();
+
+//// Email Service config
+//var emailConfig = builder.Configuration
+//        .GetSection("EmailConfiguration")
+//        .Get<EmailConfiguration>();
+//builder.Services.AddSingleton(emailConfig);
+//builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+
+//var app = builder.Build();
+
+////  check db for default user/roles. Else, seed required user/roles.
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
+//    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+//    try
+//    {
+//        var context = services.GetRequiredService<ApplicationDbContext>();
+//        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+//        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+//        await ContextSeed.SeedRolesAsync(userManager, roleManager);
+//        await ContextSeed.SeedSuperAdminAsync(userManager, roleManager);
+//    }
+//    catch (Exception ex)
+//    {
+//        var logger = loggerFactory.CreateLogger<Program>();
+//        logger.LogError(ex, "An error occurred seeding the DB.");
+//    }
+//}
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseMigrationsEndPoint();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapRazorPages();
+
+//app.Run();
